5c2784a1d4f642321cfc9f696f7b20a9319c637b
Pedro Pina <pedrolpin4@unknown.home>
2021-10-14 14:06:14 -0300

    now u can get and post categories

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ import pg from 'pg';
 
 const app = express();
 const { Pool } = pg;
-
+app.use(cors());
+app.use(express.json());
 
 const connection = new Pool ({ 
     user: 'bootcamp_role',
@@ -13,17 +14,44 @@ const connection = new Pool ({
     port: 5432,
     database: 'boardcamp'
 });
-const query = q => connection.query(q);
 
 app.get("/categories", (req, res) => {
-    query('SELECT * FROM categories;')
-        .then(res => {
-            res.send(res)
-        })
-    res.sendStatus(400)
+    const selectCategories = connection.query('SELECT * FROM categories;')
+    selectCategories.then(result => {
+        res.send(result.rows)
+    })
+    selectCategories.catch(err => res.sendStatus(400))
 })
 
-app.use(cors());
-app.use(express.json());
+app.post("/categories", (req, res) => {
+    const categoryName = req.body ? req.body.name : "";
+    let categories = [];
+    if(categoryName){
+        const selectCategories = connection.query('SELECT * FROM categories;')
+        selectCategories.then(result => {
+            categories = [...result.rows];   
+            if(categories.some(cat => cat.name === categoryName)){
+                res.sendStatus(409)
+                return
+            } else {  
+                const insertCategory = connection.query('INSERT INTO categories (name) VALUES ($1)', [categoryName])
+                insertCategory.then(result => {
+                    res.sendStatus(201)
+                    return
+                })
+                insertCategory.catch(err => {
+                     res.send(err)
+                     return  
+                })  
+            }
+        })
+        selectCategories.catch(err => {
+            res.send(err) 
+            return
+        }) 
+        console.log(categories);
+        
+    } else res.sendStatus(400)    
+})
 
 app.listen(4000);
