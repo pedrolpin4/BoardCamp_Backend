927def719b2f12f9c7b2545019aa271e0d1bcfd6
Pedro Pina <pedrolpin4@unknown.home>
2021-10-14 11:35:42 -0300

    connecting to the database

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 import express from "express"
 import cors from "cors"
+import pg from 'pg';
 
 const app = express();
+const { Pool } = pg;
+
+
+const connection = new Pool ({ 
+    user: 'bootcamp_role',
+    password: 'senha_super_hiper_ultra_secreta_do_role_do_bootcamp',
+    host: 'localhost',
+    port: 5432,
+    database: 'boardcamp'
+});
+const query = q => connection.query(q);
+
+app.get("/categories", (req, res) => {
+    query('SELECT * FROM categories;')
+        .then(res => {
+            res.send(res)
+        })
+    res.sendStatus(400)
+})
 
 app.use(cors());
 app.use(express.json());
